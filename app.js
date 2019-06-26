1a0754e1ddc3da9a2ceaa6ae624259bde95748f9
Bren <captainbmq@gmail.com>
2019-06-26 09:44:45 -0400

    more server time

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const app = express();
+
+app.get('/', function(req, res){
+    console.log('bruh')
+    res.send('gay boi');
+});
+
+app.listen(3000, function(err){
+    if (err){
+        console.log(err)
+    }
+    console.log('u mega gay times 3000')
+});
+
